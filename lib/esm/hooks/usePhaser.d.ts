5d09922eed21047d044e6e7a1d2ae026b0e1883f
Dietrich Stein <dietrich.stein@gmail.com>
2024-01-25 10:49:57 -0500

    Remove lib

--- lib/esm/hooks/usePhaser.d.ts
+++
@@ -1,3 +0,0 @@
-import { RefObject } from 'react';
-import Phaser from 'phaser';
-export default function usePhaser(config: Phaser.Types.Core.GameConfig): [RefObject<HTMLCanvasElement>, Phaser.Game | undefined];
