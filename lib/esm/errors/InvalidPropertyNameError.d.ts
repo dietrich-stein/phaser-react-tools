71eecdb8acec503965f4bd7490400304fd3b32e0
Dietrich Stein <dietrich.stein@gmail.com>
2024-01-25 10:39:52 -0500

    Removed the tests, shamefully

---
+++ lib/esm/errors/InvalidPropertyNameError.d.ts
@@ -0,0 +1,15 @@
+/**
+ * An error that occurs when an invalid property name is provided.
+ *
+ * @class
+ * @module InvalidPropertyNameError
+ * @extends TypeError
+ */
+export default class InvalidPropertyNameError extends TypeError {
+    /**
+     * Creates an instance representing an error that occurs when an invalid property name is provided.
+     *
+     * @param  {...any} [args] The error arguments.
+     */
+    constructor(...args: string[]);
+}
---
+++ lib/esm/errors/MissingArgumentError.d.ts
@@ -0,0 +1,15 @@
+/**
+ * An error that occurs when a required argument is missing.
+ *
+ * @class
+ * @module MissingArgumentError
+ * @extends TypeError
+ */
+export default class MissingArgumentError extends TypeError {
+    /**
+     * Creates an instance representing an error that occurs when a required argument is missing.
+     *
+     * @param  {...any} [args] The error arguments.
+     */
+    constructor(...args: string[]);
+}
